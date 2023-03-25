d8dc3ecdf6e3ac3a0f17305d81d97b8c543c5d1d
camipozas <cpozasg1103@gmail.com>
2023-03-24 21:42:08 -0300

    ✨ clone repositories

--- src/clone-repositories.ts
+++ src/clone-repositories.ts
@@ -1,16 +1,20 @@
-// import simpleGit, { SimpleGit } from "simple-git";
-// import { env } from "./env/env";
-
-// export const cloneRepositories = async (repoNames: string[]) => {
-//   const git: SimpleGit = simpleGit();
-
-//   for (const repoName of repoNames) {
-//     const repoUrl = `https://github.com/${env.GITHUB_ORG}/${repoName}`;
-//     const localPath = `${env.HOME}/${repoName}`;
-
-//     await git.clone(repoUrl, localPath);
-//     console.log(`Cloned ${repoName} into ${localPath}`);
-//   }
-// };
-
+import simpleGit, { SimpleGit } from "simple-git";
 import { env } from "./env/env";
+import path from "path";
+
+/**
+ * It clones a list of repositories into a local folder
+ * @param {string[]} repoNames - An array of strings that represent the names of the repositories to
+ * clone.
+ */
+export const cloneRepositories = async (repoNames: string[]) => {
+  const git: SimpleGit = simpleGit();
+
+  for (const repoName of repoNames) {
+    const repoUrl = `https://github.com/${env.GITHUB_ORG}/${repoName}`;
+    const localPath = path.join(env.HOME, env.FOLDER, repoName);
+
+    await git.clone(repoUrl, localPath);
+    console.log(`Cloned ${repoName} into ${localPath}`);
+  }
+};
