2b66efcc189031c04028c6b9b5aaf3bc13e5d424
camipozas <cpozasg1103@gmail.com>
2023-03-25 17:44:57 -0300

    ✨ add ETA

--- src/utils/progress-bar.ts
+++ src/utils/progress-bar.ts
@@ -6,27 +6,32 @@ export default class ProgressBar {
   current: number;
   barLength: number;
   name: string;
+  startTime: number;
 
   constructor(name = 'Current progress') {
     this.total = 0;
     this.current = 0;
-    this.barLength = process.stdout.columns - 40;
+    this.barLength = process.stdout.columns - 60;
     this.name = name;
+    this.startTime = Date.now();
   }
 
   init(total: number): void {
     this.total = total;
     this.current = 0;
+    this.startTime = Date.now();
     this.update(this.current);
   }
 
   update(current: number): void {
     this.current = current;
     const currentProgress = this.current / this.total;
-    this.draw(currentProgress);
+    const elapsedTime = (Date.now() - this.startTime) / 1000; // in seconds
+    const estimatedTime = (elapsedTime / currentProgress) * (1 - currentProgress); // in seconds
+    this.draw(currentProgress, estimatedTime);
   }
 
-  draw(currentProgress: number): void {
+  draw(currentProgress: number, estimatedTime: number): void {
     const filledBarLength = Math.floor(currentProgress * this.barLength);
     const emptyBarLength = this.barLength - filledBarLength;
     const nameFormatted = chalk.blueBright.bold(this.name);
@@ -34,9 +39,14 @@ export default class ProgressBar {
     const emptyBar = this.getBar(emptyBarLength, '-');
     const percentageProgress = (currentProgress * 100).toFixed(2);
 
+    const minutesRemaining = Math.floor(estimatedTime / 60);
+    const secondsRemaining = Math.floor(estimatedTime % 60);
+
     process.stdout.clearLine(0);
     process.stdout.cursorTo(0);
-    process.stdout.write(`${nameFormatted}: [${filledBar}${emptyBar}] | ${percentageProgress}%`);
+    process.stdout.write(
+      `${nameFormatted}: [${filledBar}${emptyBar}] | ${percentageProgress}% | ETA: ${minutesRemaining}m ${secondsRemaining}s`,
+    );
   }
 
   private getBar(length: number, char: string, color: (str: string) => string = (a) => a): string {
