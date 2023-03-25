dcc23ba256ab443c2c0d940520a75fc6187f14f5
camipozas <cpozasg1103@gmail.com>
2023-03-25 13:36:57 -0300

    🐛 get all pages and scroll correctly

--- src/get-repositories.ts
+++ src/get-repositories.ts
@@ -6,12 +6,15 @@ import { env } from './env/env';
 
 const log = console.log;
 
+interface Repo {
+  name: string;
+}
+
 /**
- * It makes a request to the GitHub API to get a list of all the repositories in the organization, and
- * then filters out the ones that don't start with "your-filter-prefix-" and is not empty.
- * @returns An array of strings.
+ * It gets the number of pages of repositories in the GitHub organization
+ * @returns The number of pages of repositories in the GitHub organization.
  */
-export const getAllRepositories = async () => {
+export const getPages = async () => {
   const response = await request('GET /orgs/{org}/repos', {
     org: env.GITHUB_ORG,
     headers: {
@@ -20,13 +23,49 @@ export const getAllRepositories = async () => {
     per_page: 100,
   });
 
-  const repoData = response.data;
-  if (!Array.isArray(repoData) || repoData.length === 0) {
-    return [];
+  const link = response.headers.link;
+  if (!link) {
+    return 1;
   }
 
-  const repoNames = repoData.map((repo) => repo.name);
-  return repoNames.filter((repoName: string) => repoName.startsWith('mach-'));
+  const pages = link.split(',').find((item) => item.includes('rel="last"'));
+  if (!pages) {
+    return 1;
+  }
+
+  const lastPage = pages.split(';')[0];
+  const lastPageNumber = lastPage.split('&page=')[1].split('>')[0];
+  return Number(lastPageNumber);
+};
+
+/**
+ * It makes a request to the GitHub API to get a list of all the repositories in the organization, and
+ * returns an array of the names of those repositories
+ * @param {number} pages - number - The number of pages of repositories to fetch.
+ * @returns An array of strings
+ */
+export const getAllRepositories = async (pages: number): Promise<string[]> => {
+  const repos: string[] = [];
+
+  for (let page = 1; page <= pages; page++) {
+    const response = await request('GET /orgs/{org}/repos', {
+      org: env.GITHUB_ORG,
+      headers: {
+        authorization: `token ${env.GITHUB_TOKEN}`,
+      },
+      per_page: 100,
+      page,
+    });
+
+    const repoData = response.data;
+    if (!Array.isArray(repoData) || repoData.length === 0) {
+      return [];
+    }
+
+    repos.push(...repoData.map((repo: Repo) => repo.name));
+  }
+
+  return repos;
 };
 
 /**
