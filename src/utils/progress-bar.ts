e455636e530efedeaa4e7eeb907c33e83170f7d3
camipozas <camila.pozas@banca.me>
2025-07-15 10:52:53 -0400

    Refactor getBar method parameters for improved readability

--- src/utils/progress-bar.ts
+++ src/utils/progress-bar.ts
@@ -49,7 +49,11 @@ export default class ProgressBar {
     );
   }
 
-  private getBar(length: number, char: string, color: (_str: string) => string = (_: string) => _): string {
+  private getBar(
+    length: number,
+    char: string,
+    color: (_str: string) => string = (_: string) => _,
+  ): string {
     let str = '';
     for (let i = 0; i < length; i++) {
       str += char;
